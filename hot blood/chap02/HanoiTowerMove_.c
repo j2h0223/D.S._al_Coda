39e8dd0854426690ebb308ad0eb3b910b5d54a2c
R2B <85736180+j2h0223@users.noreply.github.com>
2025-01-17 20:53:06 +0900

    fin

--- hot blood/chap02/HanoiTowerMove_.c
+++ hot blood/chap02/HanoiTowerMove_.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
-void HanoiTowerMove ();
+void HanoiTowerMove (int, char, char, char);
 
-int main(void){
+int main(void)
+{
 
-
+    HanoiTowerMove(4, 'A', 'B', 'C');
 
     return 0;
+}
+
+void HanoiTowerMove (int num, char from, char by, char to){
+    if (num == 1)
+        printf("원판 1를 %c에서 %c로 이동\n", from, to);
+    else
+    {
+        HanoiTowerMove(num - 1, from, to, by);
+        printf("원판 %d를 %c에서 %c로 이동\n", num, from, to);
+        HanoiTowerMove(num - 1, by, from, to);
+    }
 }
