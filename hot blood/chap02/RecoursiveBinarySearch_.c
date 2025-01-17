06fce236f350f967f4ef8f51808a2917b729c7cc
R2B <85736180+j2h0223@users.noreply.github.com>
2025-01-17 19:23:47 +0900

    my answer

--- hot blood/chap02/RecoursiveBinarySearch_.c
+++ hot blood/chap02/RecoursiveBinarySearch_.c
@@ -7,13 +7,15 @@ int main(void){
     int arr[] = {1,2,3,4,6,7,8,9,10};
     int result;
 
-    result = BSearchRecur(arr, 0, sizeof(arr)/sizeof(int)-1, 8);
+
+    result = BSearchRecur(arr, 0, sizeof(arr)/sizeof(int)-1, 5);
+    printf("%d\n", result);
 
     return 0;
 }
 
 int BSearchRecur(int arr[], int first, int last, int target){
-    if (first < last){
+    if (first > last){
         return -1;
     }
     else{
@@ -21,9 +23,9 @@ int BSearchRecur(int arr[], int first, int last, int target){
 
         if (arr[mid] == target)
             return target;
-        else if (arr[mid] > target)
+        else if (arr[mid] < target)
             return BSearchRecur(arr, mid+1, last, target);
         else
-            return BSearchRecur(arr, first, last-1, target);
+            return BSearchRecur(arr, first, mid-1, target);
     }
 }
